5f9018a05cf34ba885a78ccc5927264ce2d3fb63
xieyhccc <yihangxie20001013@gmail.com>
2026-07-28 21:35:36 +0100

    chapter 4 5 6

--- chapters/04_AddingASphere/main.cpp
+++ chapters/04_AddingASphere/main.cpp
@@ -4,8 +4,22 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& sphere_center, double radius, const ray& r)
+{
+    vec3 oc = sphere_center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - (radius * radius);
+
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& ray)
 {
+    if (hit_sphere(point3(0, 0, -1), 0.5, ray))
+        return color(1, 0, 0);
+
     vec3 unit_dir = unit_vector(ray.direction());
     auto a = 0.5 * (unit_dir.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
